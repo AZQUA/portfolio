2398c759d1f82d78f0ffa1b8022489104ac60d3a
Ange <leyre.ange@gmail.com>
2025-12-03 16:11:08 +0100

    Supression du miroir

---
+++ patterns/non.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Title: non
+ * Slug: bedrock/non
+ * Categories: body
+ */
+?>
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost/wordpress/wp-content/uploads/2025/12/portrait-Ange-1.jpg" alt="" class=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading">Bienvenue sur mon portfolio</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://drive.google.com/file/d/1GPQjgdP6xszYnnmsYaJ0JPXWdOrE7LNM/view?usp=drive_link">Mon CV à jour</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Mes Projets</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">L'école : IRIS</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:paragraph -->
+<p>Leyre Ange, étudiant à l'école IRIS à Montpellier sur le campus Mediaschool. Je suis développeur en alternance pour le groupe LaMeDuSe depuis le 10 septembre 2025.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
--- patterns/post-meta.php
+++ patterns/post-meta.php
@@ -4,27 +4,21 @@
  * Slug: bedrock/post-meta
  * Inserter: no
  */
-declare( strict_types = 1 );
 ?>
-
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"},"typography":{"lineHeight":"1"}},"textColor":"secondary","layout":{"type":"flex","flexWrap":"wrap"}} -->
-<div class="wp-block-group has-secondary-color has-text-color" style="line-height:1">
-	<!-- wp:image {"width":"20px","height":"20px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-	<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Diamond_Pickaxe.png" alt="" class="" style="object-fit:cover;width:20px;height:20px"/></figure>
-	<!-- /wp:image -->
+<div class="wp-block-group has-secondary-color has-text-color" style="line-height:1"><!-- wp:image {"width":"20px","height":"20px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Diamond_Pickaxe.png" alt="" style="object-fit:cover;width:20px;height:20px" /></figure>
+<!-- /wp:image -->
 
-	<!-- wp:post-date {"format":"M j, Y","isLink":true} /-->
+<!-- wp:post-date {"format":"M j, Y","isLink":true,"metadata":{"bindings":{"datetime":{"source":"core/post-data","args":{"field":"date"}}}}} /-->
 
-	<!-- wp:image {"width":"20px","height":"20px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-	<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Book.png" alt="" class="" style="object-fit:cover;width:20px;height:20px"/></figure>
-	<!-- /wp:image -->
+<!-- wp:image {"width":"20px","height":"20px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Book.png" alt="" style="object-fit:cover;width:20px;height:20px" /></figure>
+<!-- /wp:image -->
 
-	<!-- wp:post-terms {"term":"category"} /-->
+<!-- wp:image {"width":"20px","height":"20px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/Gold_Ingot.png" alt="" style="object-fit:cover;width:20px;height:20px" /></figure>
+<!-- /wp:image -->
 
-	<!-- wp:image {"width":"20px","height":"20px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-	<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Gold_Ingot.png" alt="" class="" style="object-fit:cover;width:20px;height:20px"/></figure>
-	<!-- /wp:image -->
-
-	<!-- wp:post-terms {"term":"post_tag"} /-->
-</div>
+<!-- wp:post-terms {"term":"post_tag"} /--></div>
 <!-- /wp:group -->
